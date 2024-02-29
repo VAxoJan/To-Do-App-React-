ca32c0ddfc01bc0065d9a3904acd7ebbb9c68f21
vaxo <vaxojaniashvili186@gmail.com>
2024-02-29 23:50:37 +0400

    markup

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,16 @@ function App() {
       <div className='middle'>
          <Header/>
          <MainInputStyled/>
-         <input className='checkBox' type="checkbox"></input>
+         <input className='checkBox' type="checkbox"/>
+         <div>
+          <h1>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eos, iste.
+          </h1>
+
+          <h1>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eos, iste.
+          </h1>
+         </div>
          {/* <h1>hello</h1> aq unda chavpusho valueebi  */}
       </div>
     </div>
