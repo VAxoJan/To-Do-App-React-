a9e7c2989d4e494ab090d3bf0ef5d08c9ba7445b
vaxo <vaxojaniashvili186@gmail.com>
2024-03-01 00:19:38 +0400

    value-is-coming

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
+import { useState } from 'react';
 import './App.css';
 import Header from './components/Header/Header';
 import MainInputStyled from './components/Input/MainInputStyled';
 import './responsive/responsive.css'
 
 function App() {
-
   return (
     <div className='mainMob'>
       <img className='mountainMob'/>
@@ -12,16 +12,6 @@ function App() {
          <Header/>
          <MainInputStyled/>
          <input className='checkBox' type="checkbox"/>
-         <div>
-          <h1>
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eos, iste.
-          </h1>
-
-          <h1>
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eos, iste.
-          </h1>
-         </div>
-         {/* <h1>hello</h1> aq unda chavpusho valueebi  */}
       </div>
     </div>
   )
--- src/components/Input/MainInput.jsx
+++ src/components/Input/MainInput.jsx
@@ -1,9 +1,38 @@
-import React from 'react'
+import React, { useState } from "react";
 
-const MainInput = ({width,height,radius,border,outline,margin,padding}) => {
+const MainInput = ({
+  width,
+  height,
+  radius,
+  border,
+  outline,
+  margin,
+  padding,
+}) => {
+  const [input, setInput] = useState("");
   return (
-    <input style={{width:width, height:height, borderRadius:radius, border:border, outline:outline,marginTop:margin,paddingInline:padding}} type="search" placeholder='Create a new todo...'/>
-    )
-}
+    <>
+    <input
+      onKeyDown={(e) => {
+        if (e.key === "Enter") {
+          setInput(e.target.value);
+        }
+      }}
+      style={{
+        width: width,
+        height: height,
+        borderRadius: radius,
+        border: border,
+        outline: outline,
+        marginTop: margin,
+        paddingInline: padding,
+      }}
+      type="search"
+      placeholder="Create a new todo..."
+    />
+    <h1>Value:{input}</h1>
+    </>
+  );
+};
 
 export default MainInput;
